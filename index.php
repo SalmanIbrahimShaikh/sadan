025583508a8004c85101201e8f49bbf413761aec
sadan <salmanshaikh1693@gmail.com>
2022-09-02 15:38:38 +0530

    firts upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include '../partial/header_inner.php';?>
+<div class="section-about section white-bg nomargin">
+        <div class="container">
+          
+          <div class="row clearfix common-height"  >
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-about col-middle" id="culturer-img">
+             
+             <div class="culturer-banner" style="background-color:white;"  >
+                 
+             </div> 
+             
+             
+            </div>
+          </div>
+         
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include '../partial/footer_inner.php';?>
